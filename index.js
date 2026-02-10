46aada3c27be6d11ed9a9b10e9d1f280566825a4
priyanshu jha <jhapriyanshu546@gmail.com>
2026-02-10 08:26:27 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-console.log("Hello World")
-alert ('Hello World')
-document.write('Hello World')
+//Static number
+// let a = 5
+// let b = 6
+// var c = a + b
+// console.log("The sum of "+a+" and " +b+" is: "+c)
+
+
+//Promt();
+
+let a = prompt("Plese enter first number")
+let b = prompt("Plese enter second number")
+var c = parseInt(a) + parseInt(b)
+console.log("The sum of "+a+" and "+b+" is :"+c)
