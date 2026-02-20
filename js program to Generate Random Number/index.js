2c7e5752eff36fa7bbd09697883604591505b850
priyanshu jha <jhapriyanshu546@gmail.com>
2026-02-20 07:16:49 -0800

    Add files via upload

---
+++ js program to Generate Random Number/index.js
@@ -0,0 +1,6 @@
+var x = Math.random()
+console.log("Before Calculation " + x)
+// x = x* (10-1) + 1
+x = x* 10000   //(otp kaise banate hai four digits)
+console.log("After Calculation " + x)
+console.log("After Calculation " + Math.floor(x))
