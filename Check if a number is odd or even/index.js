56bb09e056c8c9ddf58eb68366cf903eb835cfeb
priyanshu jha <jhapriyanshu546@gmail.com>
2026-02-21 19:51:58 +0530

    Add files via upload

---
+++ Check if a number is odd or even/index.js
@@ -0,0 +1,11 @@
+
+var x = 6
+// if (x % 2 == 0) {
+//     console.log(`${x} is an even number`)
+// }
+// else {
+//     console.log(`${x} is an odd number`)
+// }
+
+var res = x%2==0 ? "Even": "Odd"
+console.log(res)
